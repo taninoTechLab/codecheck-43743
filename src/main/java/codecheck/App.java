48c667dd779c87a50f4e26fe8e5800f750e1b7e9
tie303053 <tie303053@S1711C12618-T1.tisnt.tis.co.jp>
2018-06-20 16:50:47 +0900

    tets

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,6 +2,7 @@ package codecheck;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class App {
@@ -15,10 +16,10 @@ public class App {
 
 		List<Integer[]> add = new ArrayList<>();
 
-//		for (int i = 0, l = argv.length; i < l; i++) {
-//			String output = String.format("argv[%s]: %s", i, argv[i]);
-//			System.out.println(output);
-//		}
+		//		for (int i = 0, l = argv.length; i < l; i++) {
+		//			String output = String.format("argv[%s]: %s", i, argv[i]);
+		//			System.out.println(output);
+		//		}
 
 		int i = 2;
 		while (i < argv.length) {
@@ -26,42 +27,27 @@ public class App {
 			if (now_cost <= cost) { // コスト以下のものを配列に格納
 				attack.add(argv[i]);
 				requiredCost.add(argv[i + 1]);
-				add.add(new Integer[]{Integer.parseInt(argv[i]),Integer.parseInt(argv[i+1])});
+				add.add(new Integer[] { Integer.parseInt(argv[i]), Integer.parseInt(argv[i + 1]) });
 				i += 2;
 			}
 		}
 
 		Stream<Integer[]> sort2 = add.stream()
-		        .sorted((a, b) -> b[0].compareTo(a[0]));
+				.sorted((a, b) -> b[0].compareTo(a[0]));
 
+		sort2.forEach(j -> System.out.println("攻撃力:" + j[0] + " コスト:" + j[1]));
+		List<Integer[]> add_s = sort2.collect(Collectors.toList());
 
-		// コスト以下で最大の攻撃力のものを先頭に並べ替える
-//		boolean change = true;
-//		i = 0;
-//		while(change) {
-//			if(Integer.parseInt(attack.get(0)) <= Integer.parseInt(attack.get(i+1))) {
-//				String max_attack = attack.get(i+1);
-//				attack.remove(i+1);
-//				attack.add(0, max_attack);
-//
-//				String max_cost = requiredCost.get(i+1);
-//				requiredCost.remove(i+1);
-//				requiredCost.add(0, max_cost);
-//				i++;
-//			} else {
-//				change = false;
-//			}
-//		}
-
-
-//		// 先頭からコストを満たす分だけ取得する
-//		for (i = 0 ; i < attack.size(); i++) {
-////			System.out.println("攻撃力" + attack.get(i));
-////			System.out.println("コスト" + requiredCost.get(i));
-//
-//		}
-//		sort2.forEach(j -> System.out.println("攻撃力:"+j[0]+" コスト:"+j[1]));
-		add.forEach(j -> System.out.println("攻撃力:"+j[0]+" コスト:"+j[1]));
+		int result = 0;
+		i = 0;
+		while(cards > 0) {
+			if (cost > add_s.get(i)[1].intValue()) {
+				result += add_s.get(i)[0].intValue();
+				cost -= add_s.get(i)[1].intValue();
+				i++;
+			}
+		}
+		System.out.println(result);
 
 	}
 }
