70bb5efe2e6c874a73f53a5a00c88e15c93d7836
tie303053 <tie303053@S1711C12618-T1.tisnt.tis.co.jp>
2018-06-20 14:28:21 +0900

    test

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,6 +1,7 @@
 package codecheck;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class App {
@@ -9,8 +10,7 @@ public class App {
 		int cards = Integer.parseInt(argv[0]); // カード枚数
 		int cost = Integer.parseInt(argv[1]); // コスト
 
-		List<String> attack = new ArrayList<String>();
-		List<String> requiredCost = new ArrayList<String>();
+		List<String[]> status = new ArrayList<String[]>(); //[0]:attack [1]:cost
 		System.out.println(argv.length);
 
 //		for (int i = 0, l = argv.length; i < l; i++) {
@@ -22,35 +22,21 @@ public class App {
 		while (i < argv.length) {
 			int now_cost = Integer.parseInt(argv[i + 1]);
 			if (now_cost <= cost) { // コスト以下のものを配列に格納
-				attack.add(argv[i]);
-				requiredCost.add(argv[i + 1]);
+				String[] add = new String[1];
+				add[0] = argv[i];
+				add[1] = argv[i+1];
+				status.add(add);
 				i += 2;
 			}
 		}
 
 		// コスト以下で最大の攻撃力のものを先頭に並べ替える
-		boolean change = true;
-		i = 0;
-		while(change) {
-			if(Integer.parseInt(attack.get(0)) <= Integer.parseInt(attack.get(i+1))) {
-				String now_attack = attack.get(i+1);
-				attack.remove(i+1);
-				attack.set(0, now_attack);
-
-				String now_cost = requiredCost.get(i+1);
-				requiredCost.remove(i+1);
-				requiredCost.set(0, now_cost);
-				i++;
-			} else {
-				change = false;
-			}
-		}
-
+		Collections.sort(status, Collections.reverseOrder());
 
 		// 先頭からコストを満たす分だけ取得する
-		for (i = 0 ; i < attack.size(); i++) {
-			System.out.println("攻撃力" + attack.get(i));
-			System.out.println("コスト" + requiredCost.get(i));
+		for (i = 0 ; i < status.size(); i++) {
+			System.out.println("攻撃力" + status.get(i)[0]);
+			System.out.println("コスト" + status.get(i)[1]);
 
 		}
 	}
